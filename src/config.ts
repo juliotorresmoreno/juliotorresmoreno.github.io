f8f3133f24900b4ef09fed934c4aef2c3667a75a
Julio Torres <jtorres990@gmail.com>
2023-10-04 12:04:51 -0500

    feat(develop): add building state

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 export default {
   title: process.env.APP_NAME,
+  building: process.env.BUILDING,
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import { createRoot } from 'react-dom/client'
 import Root from './Root'
 import './styles/_00-root.scss'
+import config from './config'
+import React from 'react'
 
 const container = document.getElementById('app') as HTMLElement
 const root = createRoot(container)
 
-const app = Root({})
+let app
+
+if (config.building === 'false') {
+  app = Root({})
+} else {
+  app = React.createElement('h1', null, 'Building...')
+}
 
 root.render(app)
