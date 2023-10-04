b8658d1754a3ce36d8e448573d0e62af315bda07
Julio Torres <jtorres990@gmail.com>
2023-10-03 21:18:43 -0500

    initial commit

---
+++ src/Root.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import App from './components/App'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
+
+/**
+ *
+ */
+const Root: React.FC<{}> = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  )
+}
+
+export default Root
